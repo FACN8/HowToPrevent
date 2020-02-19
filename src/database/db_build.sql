68f77ef7939c7d3f47744d1ad7b28ad9f4387839
Yousef <yousefrizik92@gmail.com>
2020-02-19 11:36:53 +0200

    Adds functionality to register page

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1,33 +1,22 @@
 BEGIN;
 
-DROP TABLE IF EXISTS Users CASCADE;
-DROP TABLE IF EXISTS Profile CASCADE;
+DROP TABLE IF EXISTS usernames CASCADE;
+DROP TABLE IF EXISTS profile CASCADE;
 
-
-
-CREATE TABLE Users(
-    ID SERIAL,
-    User_Name VARCHAR(30) NOT NULL,
-    Email VARCHAR(30),
-    Password VARCHAR(30),
-    PRIMARY KEY (ID)
+CREATE TABLE usernames (
+    ID SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL
 );
 
-
-CREATE TABLE Profile(
-    ID SERIAL,
-    User_Id int NOT NULL,
-    Coins int DEFAULT 0,
-    Level_Attack int DEFAULT 1,
-    level_Auto int DEFAULT 0,
-    PRIMARY KEY (ID) 
-    FOREIGN KEY (User_Id) REFERENCES Users(ID)
+CREATE TABLE profile (
+    ID SERIAL PRIMARY KEY,
+    name_id int NOT NULL,
+    coin int DEFAULT 0,
+    level_attack int DEFAULT 1,
+    level_auto int DEFAULT 0, 
+    FOREIGN KEY (name_id) REFERENCES usernames(ID)
 );
-
-
-/*INSERT INTO Users (User_Name, Email, Password) VALUES
-('Hadeel', 'hadeel@gmail.com','123');
-
-INSERT INTO profile (user_id) VALUES ((select id from users where user_name='Hadeel') );*/
 
 COMMIT;
